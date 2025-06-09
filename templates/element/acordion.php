039fd8bf56f1dd312edd58485d4749f6e40e9aa4
Tobias Almada <talmada908@gmail.com>
2025-06-09 03:26:20 -0300

    accordio

--- templates/element/acordion.php
+++ templates/element/acordion.php
@@ -1,15 +1,22 @@
 <section>
 
-    <section style="min-height: 300px;" class="bg-primary">
-        <h5>La Solucion ideal </h5>
-        <p>TurnoFácil es la solución ideal para empresas que buscan optimizar la gestión de reservas y mejorar la experiencia del cliente. Con nuestra plataforma, podrás:
-        <ul>
-            <li>Gestionar tus reservas de manera eficiente.</li>
-            <li>Ofrecer una experiencia de usuario fluida y rápida.</li>
-            <li>Aumentar la satisfacción del cliente con un sistema de reservas fácil de usar con QRS.</li>
-            <li>Planificar mejor tu dia.</li>
-            <li>Acceder a estadísticas y reportes para mejorar tu negocio.</li>
-        </ul>
-        </p>
+    <section class="bg-primary d-flex flex-column-reverse flex-md-row flex-wrap ">
+        <div class="col col-md-9 p-4">
+
+            <h5 class="display-6 text-light fw-bold">La Solucion ideal </h5>
+            <p class="text-white">TurnoFácil es la solución ideal para empresas que buscan optimizar la gestión de reservas y mejorar la experiencia del cliente. Con nuestra plataforma, podrás:
+            <ul class="text-light">
+                <li>Gestionar tus reservas de manera eficiente.</li>
+                <li>Ofrecer una experiencia de usuario fluida y rápida.</li>
+                <li>Aumentar la satisfacción del cliente con un sistema de reservas fácil de usar con QRS.</li>
+                <li>Planificar mejor tu dia.</li>
+                <li>Acceder a estadísticas y reportes para mejorar tu negocio.</li>
+            </ul>
+            </p>
+        </div>
+        <div class="col col-md-3">
+
+            <img src="/img/shared-qr.webp" alt="QR Code" style="width: 100%; min-height: 300px;">
+        </div>
     </section>
 </section>
